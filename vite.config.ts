cf7ad7b4eb4a8bda971b9c523744f60e7513d2b0
Julian Sanchez <alejo15san95@gmail.com>
2024-01-08 18:23:43 -0500

    Creation of aliases for path
    Implemented path aliases for enhanced module import clarity and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@data': `${path.resolve(__dirname, './src/data')}`,
+      '@models': `${path.resolve(__dirname, './src/models')}`,
+      '@redux': `${path.resolve(__dirname, './src/redux')}`,
+    }
+  }
 })
