a37401654feb1917552710501c52d4b55d366c6f
Julian Sanchez <alejo15san95@gmail.com>
2024-01-08 20:41:47 -0500

    Implemented the display of selected products in the overlay
    Implemented accumulation of repeated products, added a badge displaying the total quantity of selected products, and enabled the removal of selected products.

--- src/components/features/gallery/ProductCard.tsx
+++ src/components/features/gallery/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { ImageLoader } from "@components/common";
 import { Product } from "@models/product.model";
+import { useAppDispatch } from "@redux/hooks";
+import { addProductToListOfSelectedProduct } from "@redux/slices/checkout";
 import { FC } from "react";
 
 type productCardProps = {
@@ -7,16 +9,29 @@ type productCardProps = {
 };
 
 export const ProductCard: FC<productCardProps> = ({ product }) => {
+  const dispatch = useAppDispatch();
+
+  const handleAddProductToCard = () => {
+    dispatch(addProductToListOfSelectedProduct(product));
+  };
+
   return (
     <figure className="flex flex-col animate-fade-in">
-      <ImageLoader src={product.urlImage} alt={product.name} className="rounded-t-md" />
+      <ImageLoader
+        src={product.urlImage}
+        alt={product.name}
+        className="rounded-t-md"
+      />
       <figcaption className="bg-background p-4 flex flex-col gap-2 rounded-b-md items-center">
         <h3 className="text-text text-xl text-center font-medium capitalize">
           {product.name}
         </h3>
         <p className="text-text font-bold">${product.price}</p>
         <p className="text-text">{product.description}</p>
-        <button className="text-white bg-accent py-1.5 rounded-md font-medium hover:bg-accent_hover transition-colors w-full">
+        <button
+          onClick={handleAddProductToCard}
+          className="text-white bg-accent py-1.5 rounded-md font-medium hover:bg-accent_hover transition-colors w-full"
+        >
           Add to car
         </button>
       </figcaption>
